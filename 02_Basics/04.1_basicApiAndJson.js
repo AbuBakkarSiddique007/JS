3c7538bed99e475059148d4af18568090d8ce85f
AbuBakkarSiddique007 <abubakkar.cce25.iiuc.gmail.com>
2024-07-01 01:21:41 +0600

    truthy and falsy

--- 02_Basics/04.1_basicApiAndJson.js
+++ 02_Basics/04.1_basicApiAndJson.js
@@ -3,34 +3,34 @@
 
 //API : Array of Objects (JSON) - Array of Objects 
 // Look at the following array of objects:
-[
-    {
-        name: "John",
-        age: 30
-    },
-    {
-        name: "Jane",
-        age: 25
-    },
-    {
-        name: "Jake",
-        age: 35
-    }
-];
+// [
+//     {
+//         name: "John",
+//         age: 30
+//     },
+//     {
+//         name: "Jane",
+//         age: 25
+//     },
+//     {
+//         name: "Jake",
+//         age: 35
+//     }
+// ];
 
 // API : Object of Objects (JSON) - Object of Objects
 // Look at the following object of objects:
-{
-    "John": {
-        age: 30
-    },
-    "Jane": {
-        age: 25
-    },
-    "Jake": {
-        age: 35
-    }
-}
+// {
+//     "John": {
+//         age: 30
+//     },
+//     "Jane": {
+//         age: 25
+//     },
+//     "Jake": {
+//         age: 35
+//     }
+// }
 
 
 // Sample demo:
